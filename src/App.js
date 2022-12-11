dfcd1387f00495032b7481062999e751192063fe
nicomachus <jani5714@colorado.edu>
2022-12-11 13:12:39 -0700

    playing with icons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import Box from '@mui/material/Box'
 import Avatar from '@mui/material/Avatar'
 import Button from '@mui/material/Button'
+import Link from '@mui/material/Link'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import EmailIcon from '@mui/icons-material/Email'
 const avatar = require("./static/images/avatar/jjn.jpeg")
 
 function App() {
@@ -13,9 +17,23 @@ function App() {
         <div className='header'>
           <p className='heading'>Jasper Niemeyer</p>
           <p id='sub-heading'><em>Software Engineer</em></p>
+          <div className='header-icons'>
+            <Link color="inherit" target="_blank" href="https://www.linkedin.com/in/jjniemeyer/">
+              <LinkedInIcon >LinkedIn profile</LinkedInIcon>
+            </Link>
+            <Link color="inherit" target="_blank" href="https://github.com/jjniemeyer">
+              <GitHubIcon>GitHub profile</GitHubIcon>
+            </Link>
+            <Link color="inherit" href="mailto:jasper.j.niemeyer@gmail.com">
+              <EmailIcon className='email'>send email to jasper.j.niemeyer@gmail.com</EmailIcon>
+              <div className='hidden'>Right click to copy email address</div>
+            </Link>
+            
+          </div>
         </div>
         <div className='nav'>
-          <Button variant="contained">CV</Button>
+          <Button variant="contained">Education</Button>
+          <Button variant="contained">Skills</Button>
           <Button variant="contained">Projects</Button>
           <Button variant="contained">Bio</Button>
         </div>        
